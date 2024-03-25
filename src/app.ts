f72f3122d404d619453a3f51368e5ca382f21b81
IMxSuhasTheBoy <dannylivecsr2@gmail.com>
2024-03-25 17:57:35 +0530

    Add configs for cookie-parser, cors middlewares (tested)

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,6 @@
 import express, { urlencoded } from "express";
-// import cookieParser from "cookie-parser"; ///access & set cookies from user browser (Secure cookies for server operations*), to help in perform crud operation based on those server/browser cookie session data
-// import cors from "cors"; ///Docs https://github.com/expressjs/cors#readme ///Explore config options
+import cookieParser from "cookie-parser"; ///access & set cookies from user browser (Secure cookies for server operations*), to help in perform crud operation based on those server/browser cookie session data
+import cors from "cors"; ///Docs https://github.com/expressjs/cors#readme ///Explore config options
 /***Enabling CORS lets the server tell the browser it's permitted to use an additional origin.
  *https://web.dev/articles/cross-origin-resource-sharing
  */
@@ -17,18 +17,18 @@ app.get<{}, MessageResponse>("/", (req, res) => {
 });
 
 app.use("/api/v1", routes);
-//Experimental
+///Experimental
 
-// app.use(
-//   cors({
-//     origin: process.env.CORS_ORIGIN,
-//     credentials: true,
-//   })
-// ); ///for dev phase origin is set to * , it must be changed as per deployment/hosting config
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  })
+); ///for dev phase origin is set to * , it must be changed as per deployment/hosting config
 app.use(express.json({ limit: "16kb" })); ///Docs https://expressjs.com/en/5x/api.html#express.json
 app.use(urlencoded({ extended: true, limit: "16kb" })); ///Docs https://expressjs.com/en/5x/api.html#express.urlencoded
 app.use(express.static("public")); ///Docs https://expressjs.com/en/5x/api.html#express.static
-// app.use(cookieParser()); ///Github https://github.com/expressjs/cookie-parser#readme
+app.use(cookieParser()); ///Github https://github.com/expressjs/cookie-parser#readme
 
 //TODO: routes imports
 // import userRouter from "./routes/user.routes.js";
