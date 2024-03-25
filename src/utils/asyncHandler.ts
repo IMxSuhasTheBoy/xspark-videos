7c3a5be805005dd58d97c6f64a548c82fbd83992
IMxSuhasTheBoy <dannylivecsr2@gmail.com>
2024-03-25 17:52:44 +0530

    Add utility file for asyncHandler function

--- src/utils/asyncHandler.ts
+++ src/utils/asyncHandler.ts
@@ -1,10 +1,21 @@
 /// 2.approach, returning the function in promises format
-// const asyncHandler = (reqHandler) => {
-//   return (req, res, next) => {
-//     Promise.resolve(reqHandler(req, res, next)).catch((err) => next(err));
-//   };
-// };
-// export { asyncHandler };
+import { NextFunction, Request, Response } from "express";
+
+type RequestHandler = (req: Request, res: Response, next: NextFunction) => void;
+/*@function asyncHandler @description
+  It is a higher-order function that takes a function and returns another function that
+  will call the given function with the arguments (req, res, next) wrapped in a promise,
+  and catch any error that might be thrown and pass it to the next middleware.
+ 
+  @param {RequestHandler} reqHandler Express Request Handler
+  @returns {RequestHandler} Express Request Handler with the error handling
+ */
+const asyncHandler = (reqHandler: RequestHandler): RequestHandler => {
+  return (req, res, next) => {
+    Promise.resolve(reqHandler(req, res, next)).catch((err) => next(err));
+  };
+};
+export { asyncHandler };
 
 /// 1.approach HOF: const asyncHandler = (Fn) => { async () => {trycatch} }; /// accepting a func then passing it into an func
 /*
